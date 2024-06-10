bf82c73807d0d9f853f0e8b429762873a80ed9f2
dev-strix <deyvisdjmusic@gmail.com>
2024-06-10 18:46:07 -0500

    VersionBase de este proyecto

---
+++ src/main/java/com/pj/ApiGatewayApplication.java
@@ -0,0 +1,20 @@
+package com.pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	@Bean
+    public WebClient.Builder loadBalancedWebClientBuilder() {
+        return WebClient.builder();
+    }
+}
